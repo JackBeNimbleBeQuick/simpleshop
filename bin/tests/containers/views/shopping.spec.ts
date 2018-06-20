cafb08fa6ff4754e019fee51185482101fc3043c
Jack Harrison <jack.harrison@usbank.com>
2018-06-20 07:03:17 -0700

    Stubbing out unit tests

---
+++ bin/tests/containers/views/shopping.spec.ts
@@ -0,0 +1,32 @@
+
+import Shopping from '../../../ts/containers/views/shopping';
+import FluxMehods from '../../../ts/util/fluxmethods';
+import {Fluxcom} from '../../../ts/com/Fluxcom';
+
+
+describe('Shopping unit tests', ()=>{
+
+  /*
+  .componentDidMount
+  .service call
+  .FluxMethods listen to changes
+  .Fluxcom service calls
+  */
+
+  it('defaultProps: test for props being complete and not ',()=>{});
+
+  it('componentDidMount: fluxmethods change listener',()=>{});
+
+  it('componentDidMount: fluxcom service ',()=>{});
+
+  it('select():   ',()=>{});
+
+  it('view():  ',()=>{});
+
+  it('expand():  ',()=>{});
+
+  it('renderList():  ',()=>{});
+
+  it('render():  ',()=>{});
+  
+});
